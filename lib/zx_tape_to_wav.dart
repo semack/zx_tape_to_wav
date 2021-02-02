480cd7d02601e61b9e8bfad26194d8f37a387848
Andriy S'omak <semack@gmail.com>
2021-02-02 13:04:17 +0200

    typos

--- lib/zx_tape_to_wav.dart
+++ lib/zx_tape_to_wav.dart
@@ -1,4 +1,4 @@
-/// The conversion library of .TAP/.TZX files (a ZX-Spectrum emulator data format) into .WAV sound files.
+/// Easy Flutter library to convert .TAP/.TZX files (a data format for ZX-Spectrum emulator) into sound WAV file.
 
 library zx_tape_to_wav;
 
